9384fcfee9eb9325b6ff0fcbf8a87d3eba39800c
grae ceney <gceney7@gmail.com>
2026-04-18 19:42:44 +0100

    added some funcs to easy lex and parse text without some extra boiler plate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use crate::{
+    ast::{Node, Parser},
+    token::{LexTok, Tokenizer},
+};
+
 pub mod ast;
+pub mod codegen;
 pub mod token;
 
 fn main() {
     println!("Hello, world!");
+}
+
+fn lex_text(text: &String) -> Vec<LexTok> {
+    let mut tokenizer = Tokenizer::new(text);
+    tokenizer.tokenize()
+}
+
+fn parse_tokens(tokens: Vec<LexTok>) -> Vec<Node> {
+    let mut parser = Parser::new(tokens);
+    parser.parse()
 }
